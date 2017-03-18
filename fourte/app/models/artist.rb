42697b64f653a0c1207dfcddbdced05938325a42
IrynaShv <iryna.shvydchenko@gmail.com>
2017-03-18 16:31:42 -0700

    artist

--- fourte/app/models/artist.rb
+++ fourte/app/models/artist.rb
@@ -1,2 +1,4 @@
 class Artist < ApplicationRecord
+  serialize :genres
+
 end
--- fourte/db/migrate/20170318213607_create_artists.rb
+++ fourte/db/migrate/20170318213607_create_artists.rb
@@ -2,7 +2,7 @@ class CreateArtists < ActiveRecord::Migration[5.0]
   def self.up
     create_table :artists do |t|
       t.string :name,  :null => false
-      t.string :genres, :array => true
+      t.text :genres, :array => true, :default => []
       t.string :spotify_artist_id, :null => false
 
       t.timestamps
