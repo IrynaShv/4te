034571c76adc9ade557009659dc06ffe6974ead5
Laura Bingeman <lbingeman@yahoo-inc.com>
2017-03-19 01:09:53 -0700

    Lots of view things

--- fourte/db/migrate/20170318213607_create_artists.rb
+++ fourte/db/migrate/20170318213607_create_artists.rb
@@ -2,7 +2,7 @@ class CreateArtists < ActiveRecord::Migration[5.0]
   def self.up
     create_table :artists do |t|
       t.string :name,  :null => false
-      t.text :genres, :array => true, :default => []
+      t.string :genres, :array => true
       t.string :spotify_artist_id, :null => false
 
       t.timestamps
